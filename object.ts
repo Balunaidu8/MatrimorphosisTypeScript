b349f76879e2fe0c50d7e9a03579e7ba7a47ec3f
Balu <balu@gmail.com>
2025-10-12 11:03:43 +0530

    objects added

---
+++ object.ts
@@ -0,0 +1,13 @@
+let detailofthe_owner: any = {
+  owner_name: "balu naidu",
+  owner_age: 21,
+  owner_village: "mutlur",
+  owner_dateofbirth: 2004,
+  owner_personal_info: {
+    owner_sister1_name: "keerthi",
+    owner_sister2_name: "deepthi",
+    owner_mother_name: "sam",
+    owner_father_name: "singhu",
+  },
+};
+console.log(detailofthe_owner);
